1e6ae2fab7d37a9e870fbab3fa28e5dd106085c1
Brock-99 <elmatadordelmar99@gmail.com>
2022-08-08 08:08:36 +0530

    Completed Login Form

--- src/pages/Home/Login/index.jsx
+++ src/pages/Home/Login/index.jsx
@@ -1,17 +1,111 @@
 import React, {Component} from "react";
+import {styleSheet} from "./style";
+import {withStyles} from "@mui/styles";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import GDSEButton from "../../../components/common/button";
+import GDSESnackBar from "../../../components/common/snackBar";
 
 class Login extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            username: "admin",
+            password: "admin",
+            formData: {
+                userName: "",
+                password: ""
+            },
+            //SnackBar
+            open : false,
+            massage : "",
+            severity : ""
+        }
     }
 
-    render() {
-        return (
-            <div>
+    checkValidity() {
+        console.log("Clicked")
+        console.log("User Name : " + this.state.username)
+        console.log("Password : " + this.state.password)
 
+        console.log(this.state.formData)
+        if (this.state.username == this.state.formData.userName && this.state.password == this.state.formData.password) {
+            console.log("User Name and Password Correct")
+            this.setState({
+                open: true,
+                message: "User credential matching success!",
+                severity: "success"
+            })
+        } else {
+            console.log("User Name and Password InCorrect")
+            this.setState({
+                open: true,
+                message: "User credential not matching!",
+                severity: "error"
+            })
+        }
+    }
+
+
+    render() {
+        const {classes} = this.props;
+        return (
+            <div className={classes.container}>
+                <div className={classes.loginForm_container}>
+                    <div className={classes.title_container}>
+                        <Typography variant="h4" gutterBottom component="div">
+                            Login
+                        </Typography>
+                    </div>
+                    <div className={classes.form_container}>
+                        <TextField
+                            id="outlined-basic"
+                            label="User Name"
+                            variant="outlined"
+                            onChange={(e) => {
+                                console.log(e.target.value)
+                                let formData = this.state.formData //assigning formData Object to new formData variable
+                                formData.userName = e.target.value //Add values to formData Object using event
+                                this.setState({formData}) //Set values to formData using setState
+                            }}
+                        />
+                        <TextField
+                            id="outlined-basic"
+                            type="password"
+                            label="Password"
+                            variant="outlined"
+                            onChange={(e) => {
+                                console.log(e.target.value)
+                                let formData = this.state.formData
+                                formData.password = e.target.value
+                                this.setState({formData})
+                            }}
+                        />
+                    </div>
+                    <div className={classes.btn_container}>
+                        <GDSEButton
+                            label="Login"
+                            variant="contained"
+                            onClick={() => {
+                                this.checkValidity();
+                            }}
+                        />
+                    </div>
+                </div>
+                <GDSESnackBar
+                    open={this.state.open}
+                    onClose={() => {
+                        this.setState({open: false})
+                    }}
+                    message={this.state.message}
+                    autoHideDuration={3000}
+                    severity={this.state.severity}
+                    variant="filled"
+                />
             </div>
         )
     }
+
 }
 
-export default Login
+export default withStyles(styleSheet)(Login)
---
+++ src/pages/Home/Login/style.js
@@ -0,0 +1,47 @@
+export const styleSheet = {
+    container: {
+        display:"flex",
+        flexDirection:"column",
+        flexWrap: "wrap",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "100vw",
+        height: "100vh"
+    },
+    loginForm_container:{
+        display: "flex",
+        flexWrap: "wrap",
+        flexDirection: "column",
+        height: "50%",
+        width: "30%",
+        alignItems: "center",
+        justifyContent: "center",
+        border: "black solid"
+    },
+    title_container:{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "20%"
+    },
+    form_container:{
+        display: "flex",
+        flexDirection:"column",
+        justifyContent:"space-around",
+        alignItems: "center",
+        height: "50%"
+    },
+    btn_container:{
+        display: "flex",
+        alignItems: "center",
+        justifyContent:"center",
+        height: "20%"
+    },
+    register_container:{
+        display: "flex",
+        alignItems: "center",
+        height: "10%",
+        width:'100%',
+        backgroundColor: "green"
+    }
+}
