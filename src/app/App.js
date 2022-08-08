8578c2e1bed90a7c97fab1521f7eb4ea48e95600
Brock-99 <elmatadordelmar99@gmail.com>
2022-08-08 08:55:28 +0530

    Aded 404 Page

--- src/app/App.js
+++ src/app/App.js
@@ -3,12 +3,14 @@ import Login from "../pages/Home/Login";
 import HomePage from "../pages/Home/FrontPage";
 import {Routes} from "react-router-dom";
 import {Route} from "react-router";
+import NotFound from "../pages/Home/Sessions/NotFound";
 
 function App() {
     return (
         <Routes>
             <Route excat path="/" element={<HomePage/>}/>
             <Route excat path="login" element={<Login/>}/>
+            <Route excat path="*" element={<NotFound/>}/>
         </Routes>
     );
 }
